02308a27dfede03c8c4162fcebc2a873a8b285fa
binhan <yahabin@gmail.com>
2018-11-01 23:09:23 +0800

    fewew

--- miner/models.py
+++ miner/models.py
@@ -9,18 +9,18 @@ class Miner(models.Model):
 	def __str__(self):
 		return self.name
 
-class Pool(models.Model):
-	name = models.CharField(max_length=200)
-	para1 = models.CharField(max_length=200)
-	para2 = models.CharField(max_length=200)
-	para3 = models.CharField(max_length=200)
-	para4 = models.CharField(max_length=200)
-	para5 = models.CharField(max_length=200)
-	para6 = models.CharField(max_length=200)
+# class Pool(models.Model):
+# 	name = models.CharField(max_length=200)
+# 	para1 = models.CharField(max_length=200)
+# 	para2 = models.CharField(max_length=200)
+# 	para3 = models.CharField(max_length=200)
+# 	para4 = models.CharField(max_length=200)
+# 	para5 = models.CharField(max_length=200)
+# 	para6 = models.CharField(max_length=200)
 
-	def __str__(self):
-		return self.name
+# 	def __str__(self):
+# 		return self.name
 
-class PoolMux(models.Model):
-	miner = models.ForeignKey(Miner,on_delete=models.CASCADE,default=None)
-	pool =  models.ForeignKey(Pool,on_delete=models.CASCADE,default=None)
+# class PoolMux(models.Model):
+# 	miner = models.ForeignKey(Miner,on_delete=models.CASCADE,default=None)
+# 	pool =  models.ForeignKey(Pool,on_delete=models.CASCADE,default=None)
--- miner/urls.py
+++ miner/urls.py
@@ -7,7 +7,7 @@ app_name = 'miner'
 urlpatterns = [
     re_path(r'^$', views.index, name = 'index'),
     re_path(r'^apipool', views.miner_pool.as_view(), name='api_pool'),
-    re_path(r'^(?P<miner_id>[0-9]+)/$', views.detail, name = 'detail'),
-    re_path(r'^create/$', views.miner_create, name="create"),
-    re_path(r'^change', views.change_pool, name='change'),
+    # re_path(r'^(?P<miner_id>[0-9]+)/$', views.detail, name = 'detail'),
+    # re_path(r'^create/$', views.miner_create, name="create"),
+    # re_path(r'^change', views.change_pool, name='change'),
 ]
