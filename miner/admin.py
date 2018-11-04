0280d7df07ee3fe6cdd40bc43712b43ace85d8b4
binhan <yahabin@gmail.com>
2018-11-04 11:48:19 +0800

    ewfwef

--- miner/admin.py
+++ miner/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Miner, Pool
+from .models import Miner, Pool, PoolMux
 
 admin.site.register(Miner)
 admin.site.register(Pool)
+admin.site.register(PoolMux)
