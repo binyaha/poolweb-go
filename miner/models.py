f0a0545ee347f7621ab8fa1e451c2c5ee60304fd
binhan <yahabin@gmail.com>
2018-11-04 11:29:12 +0800

    fewf

--- miner/models.py
+++ miner/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Miner(models.Model):
-	name = models.CharField(max_length=200)
-	para1 = models.CharField(max_length=200)
-	owner = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
-	pool = models .ManyToManyField(Pool, through = 'PoolMux')
-
-	def __str__(self):
-		return self.name
-
 class Pool(models.Model):
 	name = models.CharField(max_length=200)
 	para1 = models.CharField(max_length=200)
@@ -18,6 +9,15 @@ class Pool(models.Model):
 	para4 = models.CharField(max_length=200)
 	para5 = models.CharField(max_length=200)
 	para6 = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+class Miner(models.Model):
+	name = models.CharField(max_length=200)
+	para1 = models.CharField(max_length=200)
+	owner = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
+	pool = models .ManyToManyField(Pool, through = 'PoolMux')
 
 	def __str__(self):
 		return self.name
